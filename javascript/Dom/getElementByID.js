2c18ab31a49b5c81f6c571ba36f064e6201a1899
yassin <makrai.yassin@gmail.com>
2020-10-30 12:31:50 +0100

    getElementById

---
+++ javascript/Dom/getElementByID.js
@@ -0,0 +1,17 @@
+// document.getElementById()
+
+// getElementById est un sélecteur javascript qui permet de sélectioner un élément avec un "ID" du DOM (HTML)
+
+// exemple
+document.getElementById("Id_name")
+//
+// document est un mot clé qui réfère à la page HTML entière
+// getElementById est le nom de la fonction
+// c'est une fonction qui prend un seul paramètre le Id
+
+// la fonction getElementById nous rendra toujours UN seul élément
+
+// On peut donner la valeur de notre sélecteur à une variable pour permettre ensuite des manipulations
+// Exemple : 
+
+let text = document.getElementById("My_text")
