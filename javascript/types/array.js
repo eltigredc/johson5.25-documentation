cd3c3bbe8d9cafa6b674bacdcba4ae4cf200c7f0
Juliette P <juliette.palluel@gmail.com>
2020-10-30 14:50:37 +0100

    arrays

---
+++ javascript/types/array.js
@@ -0,0 +1,28 @@
+/*
+
+JS ARRAY:
+
+special type of variable that can contain many values, stored inside []
+
+=> let arr = [ value1, value2, value3] => coma separated
+
+each element can be accessed by an  index number
+first value has index [0], second [1]....
+
+exemple => console.log(arr[1])  // output here is value2
+
+
+DIFFERENCE BTW ARRAY and OBJECT :
+
+Arrays use numbered index []
+=>use Array when you want the element's name to be numbers
+
+
+Object use named index ( remember pairs of {name : value} )
+=> use object when you want the element's name to be a string ""
+
+
+
+
+
+*/
