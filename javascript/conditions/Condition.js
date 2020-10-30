4a7b57e4b2a8a85a22281a9acb4087cdbf9451de
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-30 11:08:13 +0100

    conditions

---
+++ javascript/conditions/Condition.js
@@ -0,0 +1,30 @@
+// johson5 .25 - Documentation - javascript - conditions
+
+// Conditional statements are used to perform different actions based on different conditions.
+
+// 1.if
+// It is used to run a specific block of code(if a specified condition is true).
+// Syntax:
+if (condition) {
+    //  block of code to be executed if the condition is true
+}
+
+// 2.else
+// It is used to run a specific block of code (if the same condition is false).
+//Syntax:
+if (condition) {
+    //  block of code to be executed if the condition is true
+} else {
+    //  block of code to be executed if the condition is false
+}
+
+// 3. elseif
+// It is used to specify a new condition to test (if the first condition is false).
+//Syntax:
+if (condition1) {
+    //  block of code to be executed if condition1 is true
+} else if (condition2) {
+    //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+    //  block of code to be executed if the condition1 is false and condition2 is false
+}
