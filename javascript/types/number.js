720b0983ad2f6b0eeb3e07c5cb5589fbd28dae3d
Nick-C0DE <chesnelnicolas800@gmail.com>
2020-10-30 14:15:51 +0100

    JS Doc NUMBER add type

--- javascript/types/number.js
+++ javascript/types/number.js
@@ -1,4 +1,16 @@
 
+/*Types de nombres:
+
+Nombres naturels (N), Appelés entiers positifs, Ce sont les nombres {1, 2, 3, 4, 5,…}
+Nombres entiers (W). C'est l'ensemble des nombres naturels, plus zéro {0, 1, 2, 3, 4, 5,…}.
+Entiers (Z). C'est l'ensemble de tous les nombres entiers plus tous les négatifs {⁻2, ⁻1, 0, 1, 2,…}
+Nombres rationnels (Q). Ce sont toutes les fractions où les nombres du haut et du bas sont des entiers; par exemple, 1/2, 3/4, 7/2, ⁻4 / 3, 4/1 [Remarque: le dénominateur ne peut pas être 0, mais le numérateur peut l'être].
+Nombres réels (R), Appelés nombres de mesure. Cela inclut tous les nombres qui peuvent être écrits sous forme décimale. Cela inclut les fractions écrites sous forme décimale, par exemple 0,5, 0,75 2,35, ⁻0,073, 0,3333 ou 2,142857. Il comprend également tous les nombres irrationnels tels que π, √2 etc. Chaque nombre réel correspond à un point sur la droite numérique.*/
+
+
+
+// Si on considere que dans JS tout est OBJET alors NUMBER en est uns!
+
 // L'objet JavaScript Number est un objet qui permet de travailler avec des valeurs numériques. 
 // Les principales utilisations de l'objet Number sont:
 new Number(valeur);
