b260621311fe0b1bc19dc8b5da5dcab7f59e0795
NeryCaballero <nerycaballero24@gmail.com>
2020-10-30 13:56:23 +0100

    finished

--- javascript/conditions/ifElseElseIf.js
+++ javascript/conditions/ifElseElseIf.js
@@ -15,14 +15,14 @@
         // Example :
 
                 let x = 5;
-                if ( (2+2) < x ) {
-                    console.log("This message is logged on the console BECAUSE the condition 2+2 < 5 is true.")
+                if ( 4 < x ) {
+                    console.log("This message is logged on the console BECAUSE the condition 4 < 5 is true.")
                 }
 
                 // if the condition is not true: the lines of code inside de curly brackets will not be executed.
 
-                if ( (2+2) > x ) {
-                    console.log("This message is NOT logged on the console BECAUSE the condition 2+2 > 5 is NOT true.")
+                if ( 4 > x ) {
+                    console.log("This message is NOT logged on the console BECAUSE the condition 4 > 5 is NOT true.")
                 }
 
 // ELSE : 
@@ -40,19 +40,95 @@
         // Example :
 
                 let y = 5;
-                if ( (2+2) > y ) {
-                    console.log("This message is NOT logged on the console BECAUSE the condition 2+2 > 5 is NOT true.")
+                if ( 4 > y ) {
+                    console.log("This message is NOT logged on the console BECAUSE the condition 4 > 5 is NOT true.")
                 } else {
-                    console.log("This message is logged on the console BECAUSE the condition 2+2 > 5 is FALSE.")
+                    console.log("This message is logged on the console BECAUSE the condition 4 > 5 is FALSE.")
                 }
 
 // ELSE IF :
+
         // Use else if to specify a new condition to test, if the first condition is false.
         // You can use as many 'else if' as the quantity of conditions you need to verify.
         // The first condition to become true will get excecuted, the rest will be ommited.
 
+        // Syntax :
+        
+        //    if (condition1) {
+        //          block of code to be executed if condition1 is true
+        //    } else if (condition2) {
+        //          block of code to be executed if the condition1 is false and condition2 is true
+        //    } else if (condition3) {
+        //          block of code to be executed if the condition2 is false and condition3 is true
+        //    } else {
+        //         block of code to be executed if the condition1, condition2 and condition3 are false
+        //    }
+        
+        // Example :
 
-        // W O R K   I N   P R O G R E S S ....... 
+                let a = 5;
+                if ( 4 > a ) {
+                    console.log("This message is NOT logged on the console BECAUSE the condition 4 > 5 is NOT true.");
+                } else if ( 5 > a){
+                    console.log("This message is NOT logged on the console BECAUSE the condition 5 > 5 is FALSE.");
+                } else if ( 5 == a ){
+                    console.log("This message IS logged on the console BECAUSE the condition 5 == 5 is TRUE.");
+                } else {
+                    console.log("This message is NOT logged on the console BECAUSE a previous condition was TRUE");
+                }
+                
+        // it is not necessary to end the chain of condition by an "else", it can finish by an "else if"
  
 
-// Use switch to select one of many blocks of code to be executed
+// SWITCH :        
+        // Use switch to select one of many blocks of code to be executed.
+        
+        // This is how it works:
+
+        // The switch expression is evaluated once.
+        // The value of the expression is compared with the values of each case.
+        // If there is a match, the associated block of code is executed.
+        // If there is no match, the default code block is executed. The default case does not have to be the last case in a switch block.
+        // When JavaScript reaches a break keyword, it breaks out of the switch block. This will stop the execution of inside the block. It is not necessary to break the last case in a switch block. The block breaks (ends) there anyway.
+
+        // Syntax :
+        //      switch(expression) {
+        //          case x:
+        //            code of block to be executed
+        //            break;
+        //          case y:
+        //            code of block to be executed
+        //            break;
+        //          default:
+        //            code of block to be executed
+        //      }
+
+        // Example :
+            
+            let day; 
+            
+            switch ( new Date().getDay() ) {                    // The getDay() method returns the weekday as a number between 0 and 6.
+                                                                // This example uses the weekday number to calculate the weekday name.                                                               
+                    case 0:
+                        day = "Sunday";
+                        break;
+                    case 1:
+                        day = "Monday";
+                        break;
+                    case 2:
+                        day = "Tuesday";
+                        break;
+                    case 3:
+                        day = "Wednesday";
+                        break;
+                    case 4:
+                        day = "Thursday";
+                        break;
+                    case 5:
+                        day = "Friday";
+                        break;
+                    case 6:
+                        day = "Saturday";
+            }
+
+            console.log(`Today is ${day});
